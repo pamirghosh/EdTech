66c1040233c9de98534c953361943b18aca340f9
pamirghosh <pamirghosh1998@gmail.com>
2026-02-13 16:02:34 +0530

    Set all the html files and add routes

--- app.py
+++ app.py
@@ -3,5 +3,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/our-courses")
+def courses():
+    return render_template("courses.html")
 if __name__=="__main__":
     app.run(debug=True)
