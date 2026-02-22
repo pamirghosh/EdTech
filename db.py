c3391c04870ad66aa31fd9e42b5276a64af59efb
pamirghosh <pamirghosh1998@gmail.com>
2026-02-22 10:29:24 +0530

    Modify db file

--- db.py
+++ db.py
@@ -11,7 +11,9 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
-DB_SSL_CA="D:/edtech/ca.pem"
+DB_SSL_CA=ssl={
+        "ca": os.getenv("DB_SSL_CA")
+    }
 
 engine = create_engine(
     f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
