de4d1cc8aa7fbcda2617369d4885c5304ce798ef
pamirghosh <pamirghosh1998@gmail.com>
2026-02-22 18:37:59 +0530

    modify ca file path

--- db.py
+++ db.py
@@ -11,9 +11,7 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
-DB_SSL_CA=ssl={
-        "ca": os.getenv("DB_SSL_CA")
-    }
+DB_SSL_CA=os.getenv("DB_SSL_CA")
 
 engine = create_engine(
     f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
